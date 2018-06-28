c514b44c9b6423bfd872be5cb1d5d06b69e780e8
Deliman206 <smiller206@me.com>
2018-06-28 13:17:44 -0700

    added /all route to test on postman

--- src/route/auth-router.js
+++ src/route/auth-router.js
@@ -8,7 +8,7 @@ import logger from '../lib/logger';
 const authRouter = new Router();
 const jsonParser = json();
 
-authRouter.post('/signup', (request, response, next) => { // jsonParser,
+authRouter.post('/signup', jsonParser, (request, response, next) => { 
   return Account.create(request.body.email, request.body.username, request.body.password)
     .then((account) => {
       delete request.body.password;
@@ -18,6 +18,13 @@ authRouter.post('/signup', (request, response, next) => { // jsonParser,
     .then((token) => {
       logger.log(logger.INFO, 'AUTH - returning a 200 code and a token');
       return response.json({ token });
+    })
+    .catch(next);
+});
+authRouter.get('/all', (request, response, next) => {
+  return Account.find({})
+    .then((data) => {
+      return response.json(data);
     })
     .catch(next);
 });
